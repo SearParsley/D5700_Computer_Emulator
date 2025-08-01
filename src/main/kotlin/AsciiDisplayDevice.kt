7d8aa1427beaeeb3c11ba92284d7e683a18fa975
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-08-01 00:27:23 -0600

    ASCII display device

---
+++ src/main/kotlin/AsciiDisplayDevice.kt
@@ -0,0 +1,35 @@
+class AsciiDisplayDevice(startAddress: UShort, size: UShort) : MemoryDevice(startAddress, size) {
+    private val screenBuffer = MutableList<UByte>(size.toInt()) { Constants.ASCII_DISPLAY_DEFAULT_CHAR_CODE.toUByte() }
+
+    override fun readByte(address: UShort): UByte {
+        val offset = getLocalOffset(address)
+        return screenBuffer[offset.toInt()]
+    }
+
+    override fun writeByte(address: UShort, value: UByte) {
+        val offset = getLocalOffset(address)
+        screenBuffer[offset.toInt()] = value
+    }
+
+    fun getScreenContent(): List<UByte> = screenBuffer.toList()
+
+    fun getRenderedScreen(): String {
+        val output = StringBuilder()
+        output.append("--- D5700 ASCII Display (0xF000) ---\n")
+        for (row in 0 until Constants.ASCII_DISPLAY_HEIGHT_CHARS) {
+            for (col in 0 until Constants.ASCII_DISPLAY_WIDTH_CHARS) {
+                val index = (row * Constants.ASCII_DISPLAY_WIDTH_CHARS) + col
+                val charCode = screenBuffer[index].toInt()
+
+                if (charCode >= 0x20 && charCode <= 0x7E) {
+                    output.append(charCode.toChar())
+                } else {
+                    output.append('.')
+                }
+            }
+            output.append('\n')
+        }
+        output.append("------------------------------------\n")
+        return output.toString()
+    }
+}
