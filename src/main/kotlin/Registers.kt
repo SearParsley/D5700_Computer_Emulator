08e8a9aab4338c4b65812becd09cb9b3f9e4cf41
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-07-31 21:09:02 -0600

    Create Registers.kt

---
+++ src/main/kotlin/Registers.kt
@@ -0,0 +1,14 @@
+object Registers {
+    var R0: UByte   = 0u
+    var R1: UByte   = 0u
+    var R2: UByte   = 0u
+    var R3: UByte   = 0u
+    var R4: UByte   = 0u
+    var R5: UByte   = 0u
+    var R6: UByte   = 0u
+    var R7: UByte   = 0u
+    var  P: UShort  = 0u
+    var  T: UByte   = 0u
+    var  A: UShort  = 0u
+    var  M: Boolean = false
+}
