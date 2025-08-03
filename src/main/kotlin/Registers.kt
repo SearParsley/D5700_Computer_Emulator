c994ba5bc33438d8ad93e1ccda2f12a38efbaaa7
SearParsley <161894009+SearParsley@users.noreply.github.com>
2025-08-03 01:02:43 -0600

    instruction strategies

--- src/main/kotlin/Registers.kt
+++ src/main/kotlin/Registers.kt
@@ -1,29 +1,36 @@
 object Registers {
-    var R0: UByte   = 0u
-    var R1: UByte   = 0u
-    var R2: UByte   = 0u
-    var R3: UByte   = 0u
-    var R4: UByte   = 0u
-    var R5: UByte   = 0u
-    var R6: UByte   = 0u
-    var R7: UByte   = 0u
-    var  P: UShort  = 0u
-    var  T: UByte   = 0u
-    var  A: UShort  = 0u
-    var  M: Boolean = false
+    val r: MutableList<UByte> = MutableList(8) { 0u }
+    var P: UShort  = 0u
+    var T: UByte   = 0u
+    var A: UShort  = 0u
+    var M: Boolean = false
+
+    fun setGeneralPurposeRegister(index: Int, value: UByte) {
+        if (index < 0 || index >= r.size) {
+            throw IllegalArgumentException("Invalid general purpose register index: r$index. Must be between 0 and 7.")
+        }
+        r[index] = value
+    }
+
+    fun getGeneralPurposeRegister(index: Int): UByte {
+        if (index < 0 || index >= r.size) {
+            throw IllegalArgumentException("Invalid general purpose register index: r$index. Must be between 0 and 7.")
+        }
+        return r[index]
+    }
 
     internal fun resetForTesting() {
-        R0 = 0u
-        R1 = 0u
-        R2 = 0u
-        R3 = 0u
-        R4 = 0u
-        R5 = 0u
-        R6 = 0u
-        R7 = 0u
-         P = 0u
-         T = 0u
-         A = 0u
-         M = false
+        r[0] = 0u
+        r[1] = 0u
+        r[2] = 0u
+        r[3] = 0u
+        r[4] = 0u
+        r[5] = 0u
+        r[6] = 0u
+        r[7] = 0u
+        P = 0u
+        T = 0u
+        A = 0u
+        M = false
     }
 }
